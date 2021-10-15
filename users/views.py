7ee3658710c42d0a0230c26cfeacc4f7a7d8f90e
cyber-welder <cyber-welder@ya.ru>
2021-10-15 14:24:26 +0800

    Реализован механизм регистрации пользователя и logout

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import auth
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -27,3 +27,29 @@ def login(request):
     }
 
     return render(request, 'users/login.html', context)
+
+
+def registration(request):
+
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+        else:
+            print(form.errors)
+    else:
+        form = UserRegistrationForm()
+
+    context = {
+        'title': 'GeekShop - Регистрация',
+        'form': form
+    }
+
+    return render(request, 'users/registration.html', context)
+
+
+def logout(request):
+
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
