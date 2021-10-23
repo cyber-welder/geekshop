accb372023e936023f8a6e7f0a602dc773890d59
cyber-welder <cyber-welder@ya.ru>
2021-10-23 12:12:49 +0800

    Реализовал basket_add и basket_remove через AJAX. Коряво получилось, но работает

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from datetime import datetime
 from products.models import ProductCategory, Product
-
+from baskets.models import Basket
 
 def index(request):
     context = {
@@ -15,11 +15,15 @@ def products(request):
 
     goods = Product.objects.all()
     categories = ProductCategory.objects.all()
+    baskets = {}
+    if request.user.is_authenticated:
+        baskets = {basket.product: basket.id for basket in Basket.objects.filter(user=request.user)}
 
     context = {
         'title': 'GeekShop - Каталог',
         'products': goods,
         'categories': categories,
+        'baskets': baskets,
         'copyright': datetime.now(),
     }
     return render(request, 'products/products.html', context)
--- static/js/basket.js
+++ static/js/basket.js
@@ -8,4 +8,25 @@ window.onload = function () {
             },
         })
     });
+
+    $('.product-button').on('click', 'button[action="add"]', function () {
+        var t_href = event.target;
+        $.ajax({
+            url: '/baskets/add/' + t_href.name + '/',
+            success: function (data) {
+                $('.product-button button[name="' + t_href.name + '"]').parent().html(data.result);
+            },
+        })
+    });
+
+    $('.product-button').on('click', 'button[action="remove"]', function () {
+        var t_href = event.target;
+        $.ajax({
+            url: '/baskets/remove/' + t_href.name + '/',
+            success: function (data) {
+                $('.product-button button[name="' + t_href.name + '"]').parent().html(data.result);
+            },
+        })
+    });
+
 }
